00f0f6fa47af97299f8ac02fc1d25e021ace8d02
MrocznyZdzichu <maksymzawrotny@wp.pl>
2021-02-04 23:52:42 +0100

    Simulation of LQR done

--- symulacja_temperatury_przedmiotu/sim_lqr.m
+++ symulacja_temperatury_przedmiotu/sim_lqr.m
@@ -1,4 +1,28 @@
-function [y, u] = sim_lqr(plant, conditions, Q, R)
-K =
+function [y, u] = sim_lqr(plant, conditions, Q, R, u0, y0, x0)
+%LQR setup
+u = u0;
+y = y0;
+x = x0;
+K = lqr(plant.system.A, plant.system.B, Q, R);
+
+for i = 2:length(conditions.times)
+    dui = -K*x(:, i-1);
+    ui = dui + plant.pp_Pt;
+    ui = ac_power(plant.Ph, ui);
+    ui = ui + conditions.noise_u*randn(1);
+    u = [u, ui];
+    
+    yi = step_sim(conditions.times(i-1)...
+                    , conditions.times(i)...
+                    , y(:, i-1)...
+                    , conditions.Pz(i)...
+                    , ui ...
+                    , conditions.To(i));
+    yi(2) = yi(2) + conditions.noise_y*randn(1);
+    y = [y, yi'];
+    
+    xi = yi' - [conditions.SP(i); conditions.SP(i)] ;
+    x = [x, xi];
+end
 end
 
--- symulacja_temperatury_przedmiotu/sterowanie_lqr.m
+++ symulacja_temperatury_przedmiotu/sterowanie_lqr.m
@@ -46,7 +46,7 @@ plant.pp_T =[16.8061, 16.8106];
 %% Compensation of non-measured disturbance
 conditions.times = 1:1:2000;                                %timesteps of simulations
 
-ampl = 0;                                                  %max increase of disturbance power in W 
+ampl = 30;                                                  %max increase of disturbance power in W 
 dPz = cumsum(ampl*randn(1, length(conditions.times)));      %non-measured disturbance at t
 Pz = dPz + plant.pp_Pz;
 Pz(Pz < 50) = 50;                                           %bounds keep dist between 50 W and 300 W
@@ -58,9 +58,170 @@ conditions.dPz = dPz;
 clear Pz;
 clear dPz;
 
-conditions.To = plant.pp_To*ones(1, length(conditions.times));%surrounding temperature
+conditions.To = -3*ones(1, length(conditions.times));%surrounding temperature
+conditions.noise_u = 50;
+conditions.noise_y = 0.05;
+conditions.SP = plant.pp_T(2)*ones(length(conditions.times));
 
-%LQR setup
-Q = 100;
+%we are cheating and pretenting we know all states
+u0 = plant.pp_Pt;
+y0 = plant.pp_T';
+x0 = y0 - plant.pp_T';
+
+Q = 1e8;
 R = 1;
-K = lqr(plant.system, Q, R)
+
+[y, u] = sim_lqr(plant, conditions, Q, R, u0, y0, x0);
+
+figure()
+subplot(2, 1, 1)
+plot(conditions.times, y)
+hold on
+plot(conditions.times, conditions.SP, '--')
+ylabel('Temperatura w degC')
+legend('Pomieszczenia', 'Przedmiotu', 'Wartość zadana')
+subplot(2, 1, 2)
+plot(conditions.times, u)
+hold on
+plot(conditions.times, conditions.Pz)
+legend('Z klimatyzatora', 'Zakłóceń')
+ylabel('Moc w W')
+xlabel('Czas symulacji w s')
+
+%% Tracking simulation for different R/Q ratio;
+conditions.times = 1:1:7000;                               
+
+ampl = 0;                                                  
+dPz = cumsum(ampl*randn(1, length(conditions.times)));      
+Pz = dPz + plant.pp_Pz;
+Pz(Pz < 50) = 50;                                           
+Pz(Pz > 300) = 300;
+dPz = Pz - plant.pp_Pz;
+
+conditions.Pz = Pz;
+conditions.dPz = dPz;
+clear Pz;
+clear dPz;
+
+conditions.To = -3*ones(1, length(conditions.times));%surrounding temperature
+conditions.noise_u = 50;
+conditions.noise_y = 0.05;
+conditions.SP = plant.pp_T(2)*ones(1, length(conditions.times));
+conditions.SP(200:1500) = 22;
+conditions.SP(3200:5500) = 10;
+
+%we are cheating and pretenting we know all states
+u0 = plant.pp_Pt;
+y0 = plant.pp_T';
+x0 = y0 - plant.pp_T';
+
+Q1 = 1e8;
+Q2 = 1e7;
+Q3 = 1e6;
+R = 1;
+
+[y1, u1] = sim_lqr(plant, conditions, Q1, R, u0, y0, x0);
+[y2, u2] = sim_lqr(plant, conditions, Q2, R, u0, y0, x0);
+[y3, u3] = sim_lqr(plant, conditions, Q3, R, u0, y0, x0);
+
+%% Plot the results
+figure()
+plot(conditions.times, y1(1, :))
+hold on
+plot(conditions.times, y2(1, :))
+hold on
+plot(conditions.times, y3(1, :))
+hold on
+plot(conditions.times, conditions.SP, '--')
+ylabel('Temperatura pomieszczenia w degC')
+legend(sprintf('Q/R = %d', Q1), sprintf('Q/R = %d', Q2), sprintf('Q/R = %d', Q3), 'Wartość zadana')
+%%
+figure()
+plot(conditions.times, y1(2, :))
+hold on
+plot(conditions.times, y2(2, :))
+hold on
+plot(conditions.times, y3(2, :))
+hold on
+plot(conditions.times, conditions.SP, '--')
+ylabel('Temperatura przedmiotu w degC')
+legend(sprintf('Q/R = %d', Q1), sprintf('Q/R = %d', Q2), sprintf('Q/R = %d', Q3), 'Wartość zadana')
+%%
+figure()
+plot(conditions.times, u1)
+hold on
+plot(conditions.times, u2)
+hold on
+plot(conditions.times, u3)
+legend(sprintf('Q/R = %d', Q1), sprintf('Q/R = %d', Q2), sprintf('Q/R = %d', Q3))
+ylabel('Moc w W')
+xlabel('Czas symulacji w s')
+
+%% Full simulation;
+conditions.times = 1:1:7000;                               
+
+ampl = 50;                                                  
+dPz = cumsum(ampl*randn(1, length(conditions.times)));      
+Pz = dPz + plant.pp_Pz;
+Pz(Pz < 50) = 50;                                           
+Pz(Pz > 300) = 300;
+dPz = Pz - plant.pp_Pz;
+
+conditions.Pz = Pz;
+conditions.dPz = dPz;
+clear Pz;
+clear dPz;
+
+conditions.To = -3*ones(1, length(conditions.times));%surrounding temperature
+conditions.noise_u = 50;
+conditions.noise_y = 0.05;
+conditions.SP = plant.pp_T(2)*ones(1, length(conditions.times));
+conditions.SP(200:1500) = 22;
+conditions.SP(3200:5500) = 10;
+
+%we are cheating and pretenting we know all states
+u0 = plant.pp_Pt;
+y0 = plant.pp_T';
+x0 = y0 - plant.pp_T';
+
+Q1 = 1e8;
+Q2 = 1e7;
+Q3 = 1e6;
+R = 1;
+
+[y1, u1] = sim_lqr(plant, conditions, Q1, R, u0, y0, x0);
+[y2, u2] = sim_lqr(plant, conditions, Q2, R, u0, y0, x0);
+[y3, u3] = sim_lqr(plant, conditions, Q3, R, u0, y0, x0);
+
+%% Plot the results
+figure()
+plot(conditions.times, y1(1, :))
+hold on
+plot(conditions.times, y2(1, :))
+hold on
+plot(conditions.times, y3(1, :))
+hold on
+plot(conditions.times, conditions.SP, '--')
+ylabel('Temperatura pomieszczenia w degC')
+legend(sprintf('Q/R = %d', Q1), sprintf('Q/R = %d', Q2), sprintf('Q/R = %d', Q3), 'Wartość zadana')
+%%
+figure()
+plot(conditions.times, y1(2, :))
+hold on
+plot(conditions.times, y2(2, :))
+hold on
+plot(conditions.times, y3(2, :))
+hold on
+plot(conditions.times, conditions.SP, '--')
+ylabel('Temperatura przedmiotu w degC')
+legend(sprintf('Q/R = %d', Q1), sprintf('Q/R = %d', Q2), sprintf('Q/R = %d', Q3), 'Wartość zadana')
+%%
+figure()
+plot(conditions.times, u1)
+hold on
+plot(conditions.times, u2)
+hold on
+plot(conditions.times, u3)
+legend(sprintf('Q/R = %d', Q1), sprintf('Q/R = %d', Q2), sprintf('Q/R = %d', Q3))
+ylabel('Moc w W')
+xlabel('Czas symulacji w s')
