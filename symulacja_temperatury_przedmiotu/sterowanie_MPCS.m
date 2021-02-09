68bd125f34aea6a84c2306c980e8bdb253152519
MrocznyZdzichu <maksymzawrotny@wp.pl>
2021-02-09 23:24:48 +0100

    Symulacje MPCs done

--- symulacja_temperatury_przedmiotu/sterowanie_MPCS.m
+++ symulacja_temperatury_przedmiotu/sterowanie_MPCS.m
@@ -112,3 +112,132 @@ hold on
 plot(conditions.times, conditions.Pz)
 legend(sprintf('Psi/Lambda = %d', psi1),sprintf('Psi/Lambda = %d', psi2),...
         sprintf('Psi/Lambda = %d', psi3), 'Moc zakłóceń')
+    
+%% Nadążanie
+conditions.times = 1:1:8000;  
+ampl = 0;                                                  %max increase of disturbance power in W 
+dPz = cumsum(ampl*randn(1, length(conditions.times)));      %non-measured disturbance at t
+Pz = dPz + plant.pp_Pz;
+Pz(Pz < 50) = 50;                                           %bounds keep dist between 50 W and 300 W
+Pz(Pz > 300) = 300;
+dPz = Pz - plant.pp_Pz;
+
+conditions.Pz = Pz;
+conditions.dPz = dPz;
+clear Pz;
+clear dPz;
+
+conditions.To = -10*ones(1, length(conditions.times));
+conditions.SP = plant.pp_T(2)*ones(1, length(conditions.times));
+conditions.SP(1000:3000) = 8;
+conditions.SP(4500:6200) = 24;
+
+u0 = plant.pp_Pt;
+y0 = plant.pp_T';
+
+psi1 = 1e4;
+psi2 = 1e6;
+psi3 = 1e8;
+lambda = 1e0;
+Ny = 50;
+Nu = 5;
+
+[y1, u1] = sim_MPC(plant, conditions, Ny, Nu, psi1, lambda, u0, y0);
+[y2, u2] = sim_MPC(plant, conditions, Ny, Nu, psi2, lambda, u0, y0);
+[y3, u3] = sim_MPC(plant, conditions, Ny, Nu, psi3, lambda, u0, y0);
+
+%%
+figure()
+plot(conditions.times, y1(2, :))
+hold on
+plot(conditions.times, y2(2, :))
+hold on
+plot(conditions.times, y3(2, :))
+hold on
+plot(conditions.times, conditions.SP, '--')
+legend(sprintf('Psi/Lambda = %d', psi1),sprintf('Psi/Lambda = %d', psi2),...
+        sprintf('Psi/Lambda = %d', psi3), 'Wartość zadana')
+    %%
+    figure()
+plot(conditions.times, y1(1, :))
+hold on
+plot(conditions.times, y2(1, :))
+hold on
+plot(conditions.times, y3(1, :))
+hold on
+plot(conditions.times, conditions.SP, '--')
+legend(sprintf('Psi/Lambda = %d', psi1),sprintf('Psi/Lambda = %d', psi2),...
+        sprintf('Psi/Lambda = %d', psi3), 'Wartość zadana')
+    %%
+figure()
+plot(conditions.times, u1)
+hold on
+plot(conditions.times, u2)
+hold on
+plot(conditions.times, u3)
+hold on
+plot(conditions.times, conditions.Pz)
+legend(sprintf('Psi/Lambda = %d', psi1),sprintf('Psi/Lambda = %d', psi2),...
+        sprintf('Psi/Lambda = %d', psi3), 'Moc zakłóceń')
+    
+%% Nadążanie + zaklócenia
+conditions.times = 1:1:8000;  
+ampl = 10;                                                  %max increase of disturbance power in W 
+dPz = cumsum(ampl*randn(1, length(conditions.times)));      %non-measured disturbance at t
+Pz = dPz + plant.pp_Pz;
+Pz(Pz < 50) = 50;                                           %bounds keep dist between 50 W and 300 W
+Pz(Pz > 300) = 300;
+dPz = Pz - plant.pp_Pz;
+
+conditions.Pz = Pz;
+conditions.dPz = dPz;
+clear Pz;
+clear dPz;
+
+u0 = plant.pp_Pt;
+y0 = plant.pp_T';
+
+psi1 = 1e4;
+psi2 = 1e6;
+psi3 = 1e8;
+lambda = 1e0;
+Ny = 50;
+Nu = 5;
+
+[y1, u1] = sim_MPC(plant, conditions, Ny, Nu, psi1, lambda, u0, y0);
+[y2, u2] = sim_MPC(plant, conditions, Ny, Nu, psi2, lambda, u0, y0);
+[y3, u3] = sim_MPC(plant, conditions, Ny, Nu, psi3, lambda, u0, y0);
+
+%%
+figure()
+plot(conditions.times, y1(2, :))
+hold on
+plot(conditions.times, y2(2, :))
+hold on
+plot(conditions.times, y3(2, :))
+hold on
+plot(conditions.times, conditions.SP, '--')
+legend(sprintf('Psi/Lambda = %d', psi1),sprintf('Psi/Lambda = %d', psi2),...
+        sprintf('Psi/Lambda = %d', psi3), 'Wartość zadana')
+    %%
+    figure()
+plot(conditions.times, y1(1, :))
+hold on
+plot(conditions.times, y2(1, :))
+hold on
+plot(conditions.times, y3(1, :))
+hold on
+plot(conditions.times, conditions.SP, '--')
+legend(sprintf('Psi/Lambda = %d', psi1),sprintf('Psi/Lambda = %d', psi2),...
+        sprintf('Psi/Lambda = %d', psi3), 'Wartość zadana')
+    %%
+figure()
+plot(conditions.times, u1)
+hold on
+plot(conditions.times, u2)
+hold on
+plot(conditions.times, u3)
+hold on
+plot(conditions.times, conditions.Pz)
+legend(sprintf('Psi/Lambda = %d', psi1),sprintf('Psi/Lambda = %d', psi2),...
+        sprintf('Psi/Lambda = %d', psi3), 'Moc zakłóceń')
